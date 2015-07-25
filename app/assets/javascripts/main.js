fa5368a81e732ed41102c5611173e6df088f98af
Trent Gardner II <trent.gardner.ii@gmail.com>
2015-07-25 18:31:56 +0000

    Added bkgrd video to firstPage - need to refine

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -21,13 +21,57 @@ $(document).ready(function(){
          verticalCentered: true,
           fixedElements:'#tf-menu', 
          sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000'],
+         scrollOverflow: false,
          afterRender: function(){
             $('.fp-section.fp-table').css('display','flex');
+         $('#bgvid').get(0).play();
   
         }
          
          
        });
+  
+  function iedetect(v) {
+	    var r = RegExp('msie' + (!isNaN(v) ? ('\\s' + v) : ''), 'i');
+		return r.test(navigator.userAgent);
+			
+	}
+
+
+		// Wait until the video meta data has loaded
+		$('#bgvid video').on('loadedmetadata', function() {
+			
+			var $width, $height, // Width and height of screen
+				$vidwidth = this.videoWidth, // Width of video (actual width)
+				$vidheight = this.videoHeight, // Height of video (actual height)
+				$aspectRatio = $vidwidth / $vidheight; // The ratio the video's height and width are in
+						
+			(adjSize = function() { // Create function called adjSize
+							
+				$width = $(window).width(); // Width of the screen
+				$height = $(window).height(); // Height of the screen
+							
+				$boxRatio = $width / $height; // The ratio the screen is in
+							
+				$adjRatio = $aspectRatio / $boxRatio; // The ratio of the video divided by the screen size
+							
+				// Set the container to be the width and height of the screen
+				$('#section0').css({'width' : $width+'px', 'height' : $height+'px'}); 
+							
+				if($boxRatio < $aspectRatio) { // If the screen ratio is less than the aspect ratio..
+					// Set the width of the video to the screen size multiplied by $adjRatio
+					$vid = $('#section0 video').css({'width' : $width*$adjRatio+'px'}); 
+				} else {
+					// Else just set the video to the width of the screen/container
+					$vid = $('#section0 video').css({'width' : $width+'px'});
+				}
+								 
+			})(); // Run function immediately
+						
+			// Run function also on window resize.
+			$(window).resize(adjSize);
+		
+		});
       
  /* ==============================================
   	Page Scroll
